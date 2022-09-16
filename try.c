361f2f3bb173705c01391ff137b749476fab9f38
mRrobots <mosismdlalose@gmail.com>
2022-09-16 18:30:53 +0000

    v5

---
+++ try.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // freopen("lol.txt","w",stdout);
+    // printf("Send it to lol right now. ");
+    // fclose(stdout);
+    int i =1;
+
+    if(i){
+        printf("bool works\n");
+    }
+    else{
+        printf("Na\n");
+    }
+
+    return(0);
+}
--- witsshell.c
+++ witsshell.c
@@ -6,6 +6,7 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <limits.h>
+#include <fcntl.h>
 
 int batchmode = 0;
 void int_mode(){
@@ -18,67 +19,59 @@ void int_mode(){
     char *dir;
     char *to; 
     int s;
-    int rc;                                        
+    int rc;
+    char *paths[1024];
+    // char *path[1024];
+    int pathsize = 1;
+    
+                                           
 
     /*while no exit or CTRL_D*/
     do{
+        paths[0] = "/bin/";
         size_t buffersize = 32;    
         buffer = (char*)malloc(buffersize*sizeof(char));    //declare
         printf("witsshell> ");                              //prompt
-        read = getline(&buffer,&buffersize,stdin);          //user input
+        read = getline(&buffer,&buffersize,stdin);         //user input
         /*this will help to not change the user input*/
         char *buffbrother = buffer;                         //hey brother
         /*for spacing multiple reads*/
         char *found;                                        
-        char **arg = malloc(buffersize*sizeof(char*));                              //data structure :/
+        char **arg = malloc(buffersize*sizeof(char*)); 
+                                     //data structure :/
         int pos = 0;                                        //num of arg
         found = strsep(&buffbrother," "); 
         /*getting user input from buffer 
          and separating em and storing 
           them to  array DS
-        */ 
-        while (found != NULL )
+        */
+       int redirection = 0; 
+        while ( found != NULL )
         {
             arg[pos] = found;
+            char *first[1];
+            first[0] = arg[pos];
             pos++;
 
             if((int)pos >= (int)buffersize){
                 buffersize+=buffersize;
                 arg = realloc(arg,buffersize*sizeof(char*));
             }
+            if(strcmp(first[0],">") == 0){  //check if i have to rerout to the output file
+                redirection = 1; //yep we have to, but becareful
+            }
             // printf("%s+%s\n",found,arg[pos]);
             found = strsep(&buffbrother," ");
         }
+
         arg[pos] = NULL;
+        
 
         char *first[1];
         first[0] = arg[0];
         char * bin;
-        if(!strcmp(first[0],"echo\n") || !strcmp(first[0],"echo")){
-            bin = "/bin/echo";
-            int s;
-            int rc = fork();
-            if(rc==0){
-                execv("/bin/echo",arg);
-            }
-            else{
-                wait(&s);
-            }     
-        }
 
-        else if(!strcmp(first[0],"ls\n") || !strcmp(first[0],"ls")){
-            bin = "/bin/ls";
-            int s;
-            int rc = fork();
-            if(rc==0){
-                execv("/bin/ls",arg);
-            }
-            else{
-                wait(&s);
-            }     
-        }
-
-        else if(!strcmp(first[0],"exit\n") || !strcmp(first[0],"exit")){
+        if(!strcmp(first[0],"exit\n") || !strcmp(first[0],"exit")){
             exit(0);  
         }
 
@@ -100,18 +93,70 @@ void int_mode(){
                 wait(&s);
                 exit(1);
             }     
-            
         }
-        // printf("%s\n",bin);
-        // rc = fork();
-        // if(rc==0){
-        //     execv(bin,arg);            
-        // }
-        // else{
-        //     wait(&s);
-        // } 
-        //free(buffer);
-        //free(arg);  
+        else if(!strcmp(first[0],"path")){
+            pathsize = 1;
+            for(int i=1;i<pos;i++){
+                paths[i] = arg[i];
+                pathsize++;
+            }
+        }
+
+        else{
+            for(int i = 0;i<pathsize;i++){
+                char dest[128];
+                char dest2[128];
+                char *path = paths[i];
+                char *a = first[0];
+                strtok(a,"\n");
+                /*printf("%s\n",path);
+                printf("%s\n",a);*/
+                strcpy(dest,path);
+                strcpy(dest2,a);
+                /*printf("%s\n",dest);
+                printf("%s\n",a);*/
+                strcat(dest,dest2);
+                // printf("%s\n",dest);
+                // strcat(path,a);
+                int fd = access(dest,X_OK);
+                if( fd ==0){
+                    if(redirection){
+                        //write to a file
+                        int rc = fork();
+                        int fd = open("mosis.txt",O_RDWR|O_CREAT|O_APPEND,777);
+                        if(rc==0){
+                            //child
+                            close(STDIN_FILENO);
+                            dup2(fd,1);
+                            printf("write something");
+                            close(fd);
+                            execv(dest,arg);
+                        }
+                        else{
+                            wait(&s);
+                            close(fd);
+                        }
+                    }
+                    else{
+                        int rc = fork();
+                        if(rc==0){
+                            execv(dest,arg);
+                        }
+                        else{
+                            wait(&s);
+                        }     
+                    }
+                    
+                }
+                else{
+                    // printf("nahh\n");
+                    //its an error
+                }
+            }
+        }
+
+        // pos = 0;
+        // free(arg);        
     }
     while(strcmp(buffer,"exit\n") &&!feof(stdin));
 }
